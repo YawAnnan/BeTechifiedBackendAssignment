2eabbf8a0ec479f477764c0d19764c7e93672108
YawAnnan <annanamoahisaac585@outlook.com>
2026-06-12 21:26:32 +0000

    Initial commit: Add intro.js

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Isaac"
+const age = 26
+const favouriteLanguage = "JavaScript"
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favorite Programming Language: ${favouriteLanguage}`);
